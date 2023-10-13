f0b3ccaba8f7074a035549697ee06a723cf4a6da
Alex Eduardo <alexeduardoaeas@gmail.com>
2023-10-13 00:00:11 -0300

    Ajustes de comentarios

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 
+/**
+ * Classe que recebe dois parâmetros inteiros e imprime todos os números entre
+ * eles
+ * 
+ * @autor: AlexEduardo-zip
+ * @version 1.0
+ * @since 2021-03-01
+ */
 public class Contador {
     public static void main(String[] args) throws Exception {
 
@@ -9,10 +17,11 @@ public class Contador {
         System.out.println("Digite o segundo parâmetro");
         int parametroDois = terminal.nextInt();
 
+        // try-catch
         try {
             // chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
-
+            // capturando a exceção do tipo ParametrosInvalidosException
         } catch (ParametrosInvalidosException e) {
             // imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
@@ -22,11 +31,21 @@ public class Contador {
 
     }
 
+    /**
+     * Método que recebe dois parâmetros inteiros e imprime todos os números entre
+     * eles
+     * 
+     * @param parametroUm
+     * @param parametroDois
+     * @throws ParametrosInvalidosException
+     */
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         if (parametroUm >= parametroDois) {
+            // lançar uma exceção do tipo ParametrosInvalidosException
             throw new ParametrosInvalidosException();
         }
 
+        // imprimir todos os números entre os parâmetros
         int contagem = parametroDois - parametroUm;
         for (int i = 0; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
